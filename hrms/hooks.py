085627f5236721fe4f381a20048b790d8766463d
zayatsikrok <126092310+zayatsikrok@users.noreply.github.com>
2024-01-10 15:43:52 +0200

    add progressive method to daily task

--- hrms/hooks.py
+++ hrms/hooks.py
@@ -191,6 +191,7 @@ scheduler_events = {
 		"hrms.controllers.employee_reminders.send_work_anniversary_reminders",
 		"hrms.hr.doctype.daily_work_summary_group.daily_work_summary_group.send_summary",
 		"hrms.hr.doctype.interview.interview.send_daily_feedback_reminder",
+		"hrms.controllers.employee_reminders.send_reminders_in_advance_progressive"
 	],
 	"daily_long": [
 		"hrms.hr.doctype.leave_ledger_entry.leave_ledger_entry.process_expired_allocation",
