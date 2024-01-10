a67ea886e9bf235596423c06e6d403b6216ad55b
zayatsikrok <126092310+zayatsikrok@users.noreply.github.com>
2024-01-10 13:03:37 +0200

    add attendance calendar changes

--- hrms/hr/doctype/attendance/attendance_calendar.js
+++ hrms/hr/doctype/attendance/attendance_calendar.js
@@ -1,12 +1,27 @@
 // Copyright (c) 2018, Frappe Technologies Pvt. Ltd. and contributors
 // For license information, please see license.txt
+
 frappe.views.calendar["Attendance"] = {
 	options: {
 		header: {
-			left: 'prev,next today',
-			center: 'title',
-			right: 'month'
-		}
+			left: "prev, title, next",
+			right: "today, month, agendaWeek, agendaDay",
+		},
+		
+		displayEventTime: false,
+		
 	},
-	get_events_method: "hrms.hr.doctype.attendance.attendance.get_events"
-};
+	field_map: {
+		start: "attendance_date",
+		end: "attendance_date",
+		title: "employee_name",
+		allDay: 1,
+		
+	},
+
+	other_fields: [
+		"status",
+		"leave_type"
+	],
+	get_events_method: 'frappe.desk.calendar.get_events'
+}
