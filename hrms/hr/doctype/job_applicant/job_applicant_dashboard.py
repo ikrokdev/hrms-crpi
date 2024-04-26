7e34f7c54410f8e2de8e13fdd01a60f785ef145a
popenko-ikrok <popenko.d@ikrok.net>
2024-04-26 17:09:16 +0300

    fix(Job Applicant): edit dashbord

--- hrms/hr/doctype/job_applicant/job_applicant_dashboard.py
+++ hrms/hr/doctype/job_applicant/job_applicant_dashboard.py
@@ -2,8 +2,8 @@ def get_data():
 	return {
 		"fieldname": "job_applicant",
 		"transactions": [
-			{"items": ["Employee", "Employee Onboarding"]},
-			{"items": ["Job Offer", "Appointment Letter"]},
 			{"items": ["Interview"]},
+			{"items": ["Job Offer"]},
+			{"items": ["Employee"]},
 		],
 	}
